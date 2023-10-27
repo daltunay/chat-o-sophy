6644ac721d65c1d34884c394ce2954ac9ea19e60
Daniel Altunay <daniel.altunay@decathlon.com>
2023-10-27 15:47:11 +0200

    improve api management

--- pages/1_💬_single_mode.py
+++ pages/1_💬_single_mode.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 from chatbots import SingleChatbot
 from utils.api_key import configure_openai_api_key
-from utils.chat_history import display_history, enable_user_input, reset
+from utils.chat_history import display_history
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,23 +19,18 @@ st.write(
 )
 
 
-@reset
 @display_history
 def main():
     configure_openai_api_key()
+
     if "chatbot" not in st.session_state:
-        st.session_state.chatbot = SingleChatbot()
+        st.session_state.chatbot = SingleChatbot(philosopher="Nietzsche")
         st.session_state.history = []
 
-    enable_user_input(
-        st.session_state.get("valid_openai_api_key")
-        or st.session_state.get("community_version")
-    )
     if user_query := st.chat_input(
         placeholder="Ask me anything!",
-        disabled=not st.session_state.input_enabled,
-        on_submit=enable_user_input,
-        kwargs={"state": False},
+        disabled=st.session_state.get("chatbot") is None
+        or st.session_state.get("OPENAI_API_KEY") is None,
     ):
         st.session_state.chatbot.chat(user_query)
 
--- utils/api_key.py
+++ utils/api_key.py
@@ -17,51 +17,54 @@ def configure_openai_api_key():
     st.sidebar.header("OpenAI API")
 
     # Community version checkbox
-    if st.sidebar.checkbox(
+    st.sidebar.checkbox(
         label="Community Version",
         key="community_version",
-        value=st.session_state.get(
-            "community_version", st.session_state.get("_community_version", False)
-        ),
-    ):
-        os.environ["OPENAI_API_KEY"] = os.getenv("LOCAL_OPENAI_API_KEY")
-    st.session_state._community_version = st.session_state.community_version  # memorize
+        value=st.session_state.get("_community_version", False),
+        on_change=check_openai_api_key,
+    )
 
     # API key text input
     st.sidebar.text_input(
         label="OpenAI API Key",
         type="password",
         value=""
-        if st.session_state.get("community_version")
-        else st.session_state.get("_OPENAI_API_KEY", ""),
+        if st.session_state.get("_community_version")
+        else st.session_state.get("_USER_OPENAI_API_KEY", ""),
         placeholder="sk-...",
         autocomplete="",
-        key="OPENAI_API_KEY",
-        disabled=st.session_state.get("community_version", False),
+        key="USER_OPENAI_API_KEY",
+        disabled=st.session_state.get("_community_version", False),
         on_change=check_openai_api_key,
     )
-    if st.session_state.get("valid_openai_api_key"):
-        st.session_state._OPENAI_API_KEY = st.session_state.OPENAI_API_KEY  # memorize
-        os.environ["OPENAI_API_KEY"] = st.session_state.OPENAI_API_KEY
 
-    # Toast
-    if not (
-        st.session_state.get("community_version")
-        or st.session_state.get("valid_openai_api_key")
-    ):
+    if not st.session_state.get("_OPENAI_API_KEY"):
         st.error("Please add your OpenAI API key to continue.")
         st.info("Obtain your key from: https://platform.openai.com/account/api-keys")
-        st.stop()
+        _ = os.environ.pop("OPENAI_API_KEY", None)
 
 
 def check_openai_api_key():
-    openai.api_key = st.session_state.get("OPENAI_API_KEY")
+    logging.info("Checking OpenAI API key")
+    st.session_state._community_version = st.session_state.get("community_version")
+    st.session_state._USER_OPENAI_API_KEY = st.session_state.get("USER_OPENAI_API_KEY")
+
+    openai.api_key = (
+        os.getenv("LOCAL_OPENAI_API_KEY")
+        if st.session_state.get("_community_version")
+        else st.session_state.get("_USER_OPENAI_API_KEY")
+    )
+
     try:
         _ = openai.Model.list()
         logging.info("Authentication successful!")
         st.toast("Authentication successful!", icon="✅")
-        st.session_state.valid_openai_api_key = True
+        st.session_state.OPENAI_API_KEY = openai.api_key
+        os.environ["OPENAI_API_KEY"] = st.session_state.OPENAI_API_KEY
+
     except openai.error.AuthenticationError:
         logging.error("Authentication error")
         st.toast("Authentication error", icon="🚫")
-        st.session_state.valid_openai_api_key = False
+        st.session_state.OPENAI_API_KEY = None
+
+    st.session_state._OPENAI_API_KEY = st.session_state.OPENAI_API_KEY  # memorize
