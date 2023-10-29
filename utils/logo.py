b8f22c6833de646c399f59bab7d5f9cc91444d8f
Daniel Altunay <daniel.altunay@decathlon.com>
2023-10-29 15:16:10 +0100

    improve logo

--- utils/logo.py
+++ utils/logo.py
@@ -7,9 +7,7 @@ import openai
 @st.cache_resource(show_spinner="Generating logo...")
 def generate_logo():
     image = openai.Image.create(
-        prompt="A logo representing a cartoon robot with a mustache, wearing glasses and holding a book."
-        "The robot has a thought bubble above its head containing a lightbulb symbolizing ideas and knowledge."
-        "This logo is designed for a chatbot philosophy application.",
+        prompt="Picture a logo, showing a pondering robot with a lightbulb over its head.",
         n=1,
         size="512x512",
         api_key=os.getenv("LOCAL_OPENAI_API_KEY"),
