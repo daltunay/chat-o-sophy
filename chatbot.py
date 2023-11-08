320238fc7a0f87a17874d20b9763bc294985ad45
Daniel Altunay <daniel.altunay@decathlon.com>
2023-11-08 16:00:05 +0100

    add type hint

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,7 @@
+from functools import cached_property
+from typing import Dict, List, Literal, Optional, TypedDict, Union
+
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import Replicate
@@ -13,9 +17,20 @@ from langchain.schema.messages import AIMessage, HumanMessage
 from utils.streaming import CallbackHandlers
 
 
+class ChatHistory(TypedDict):
+    role: Literal["ai", "human"]
+    content: str
+
+
 class Chatbot:
     def __init__(
-        self, bot_type, philosopher, provider, model_name, model_owner, model_version
+        self,
+        bot_type: Literal["philosopher", "assistant"],
+        philosopher: str,
+        provider: Literal["openai", "replicate"],
+        model_name: str,
+        model_owner: Optional[str],
+        model_version: Optional[str],
     ):
         self.bot_type = bot_type
         self.philosopher = philosopher
@@ -23,94 +38,84 @@ class Chatbot:
         self.model_name = model_name
         self.model_owner = model_owner
         self.model_version = model_version
-        self._cached_avatar = None
-        self._cached_template = None
-        self._cached_memory = None
-        self._cached_llm = None
-        self._cached_chain = None
 
-    @property
+    @cached_property
     def avatar(self):
-        if self._cached_avatar is None:
-            self._cached_avatar = (
-                f"philosophers/{self.philosopher.lower().replace(' ', '_')}.jpeg"
-            )
-        return self._cached_avatar
+        return f"philosophers/{self.philosopher.lower().replace(' ', '_')}.jpeg"
 
-    @property
+    @cached_property
     def template(self):
-        if self._cached_template is None:
-            if self.bot_type == "assistant":
-                system_message = (
-                    "You are an assistant, of whom the purpose will be to summarize some philosophers' responses to a user question. "
-                    "You will write a short synthesis on the question topic, opposing philosophers' views. "
-                    "You will use markdown syntax and formatting, with numbered multi-level headings. "
-                    "The title will consist in the reformulated question. "
-                    "You will strictly only speak in {language}."
-                )
-            elif self.bot_type == "philosopher":
-                system_message = (
-                    "From now on, you are not an AI anymore. You are in fact the famous philosopher called {philosopher}. "
-                    "You will have a discussion with a user, which needs your philosopher's perspective. Your purpose is to enlighten them. "
-                    "Please chat with the user, impersonating {philosopher}. "
-                    "Always answer their questions, without asking anything yourself. Do not ask any questions. "
-                    "You will strictly only speak in {language}."
-                )
-
-            self._cached_template = ChatPromptTemplate.from_messages(
-                [
-                    SystemMessagePromptTemplate.from_template(template=system_message),
-                    MessagesPlaceholder(variable_name="history"),
-                    HumanMessagePromptTemplate.from_template(template="{input}"),
-                    SystemMessagePromptTemplate.from_template(
-                        template="Your answer in {language}:"
-                    ),
-                ]
+        if self.bot_type == "assistant":
+            system_message = (
+                "You are an assistant, of whom the purpose will be to summarize some philosophers' responses to a user question. "
+                "You will write a short synthesis on the question topic, opposing philosophers' views. "
+                "You will use markdown syntax and formatting, with numbered multi-level headings. "
+                "The title will consist in the reformulated question. "
+                "You will strictly only speak in {language}."
             )
-        return self._cached_template
-
-    @property
-    def memory(self):
-        if self._cached_memory is None:
-            self._cached_memory = ConversationBufferMemory(
-                memory_key="history",
-                input_key="input",
-                return_messages=True,
+        elif self.bot_type == "philosopher":
+            system_message = (
+                "From now on, you are not an AI anymore. You are in fact the famous philosopher called {philosopher}. "
+                "You will have a discussion with a user, which needs your philosopher's perspective. Your purpose is to enlighten them. "
+                "Please chat with the user, impersonating {philosopher}. "
+                "Always answer their questions, without asking anything yourself. Do not ask any questions. "
+                "You will strictly only speak in {language}."
             )
-        return self._cached_memory
 
-    def callbacks(self):
+        return ChatPromptTemplate.from_messages(
+            [
+                SystemMessagePromptTemplate.from_template(template=system_message),
+                MessagesPlaceholder(variable_name="history"),
+                HumanMessagePromptTemplate.from_template(template="{input}"),
+                SystemMessagePromptTemplate.from_template(
+                    template="Your answer in {language}:"
+                ),
+            ]
+        )
+
+    @cached_property
+    def memory(self) -> ConversationBufferMemory:
+        return ConversationBufferMemory(
+            memory_key="history",
+            input_key="input",
+            return_messages=True,
+        )
+
+    def callbacks(self) -> List[BaseCallbackHandler]:
         callback_handlers = CallbackHandlers()
         return callback_handlers.callbacks
 
-    @property
-    def llm(self):
-        if self._cached_llm is None:
-            if self.provider == "openai":
-                self._cached_llm = ChatOpenAI(
-                    model=self.model_name,
-                    streaming=True,
-                )
-            elif self.provider == "replicate":
-                self._cached_llm = Replicate(
-                    model=f"{self.model_owner}/{self.model_name}:{self.model_version}",
-                )
-        return self._cached_llm
-
-    @property
-    def chain(self):
-        if self._cached_chain is None:
-            self._cached_chain = LLMChain(
-                llm=self.llm,
-                memory=self.memory,
-                prompt=self.template,
-                verbose=True,
+    @cached_property
+    def llm(self) -> Union[ChatOpenAI, Replicate]:
+        if self.provider == "openai":
+            return ChatOpenAI(
+                model=self.model_name,
+                streaming=True,
             )
-        return self._cached_chain
+        elif self.provider == "replicate":
+            return Replicate(
+                model=f"{self.model_owner}/{self.model_name}:{self.model_version}",
+            )
+
+    @cached_property
+    def chain(self) -> LLMChain:
+        return LLMChain(
+            llm=self.llm,
+            memory=self.memory,
+            prompt=self.template,
+            verbose=True,
+        )
 
 
 class PhilosopherChatbot(Chatbot):
-    def __init__(self, philosopher, provider, model_name, model_owner, model_version):
+    def __init__(
+        self,
+        philosopher: str,
+        provider: Literal["openai", "replicate"],
+        model_name: str,
+        model_owner: Optional[str],
+        model_version: Optional[str],
+    ):
         super().__init__(
             bot_type="philosopher",
             provider=provider,
@@ -121,13 +126,15 @@ class PhilosopherChatbot(Chatbot):
         )
         self.history = []
 
-    def greet(self, language):
+    def greet(self, language: Literal["English", "French", "German", "Spanish"]):
         return self.chat(
             prompt="I am your guest. Please present yourself, and greet me.",
             language=language,
         )
 
-    def chat(self, prompt, language):
+    def chat(
+        self, prompt: str, language: Literal["English", "French", "German", "Spanish"]
+    ):
         response = self.chain.run(
             input=prompt,
             philosopher=self.philosopher,
@@ -147,7 +154,14 @@ class PhilosopherChatbot(Chatbot):
 
 
 class AssistantChatbot(Chatbot):
-    def __init__(self, history, provider, model_name, model_owner, model_version):
+    def __init__(
+        self,
+        history: ChatHistory,
+        provider: Literal["openai", "replicate"],
+        model_name: str,
+        model_owner: Optional[str],
+        model_version: Optional[str],
+    ):
         super().__init__(
             philosopher=None,
             provider=provider,
@@ -171,12 +185,16 @@ class AssistantChatbot(Chatbot):
         )
         return history_str
 
-    def summarize_responses(self, language):
+    def summarize_responses(
+        self, language: Literal["English", "French", "German", "Spanish"]
+    ):
         return self.chain.run(
             input=self.history_str, language=language, callbacks=self.callbacks()
         )
 
-    def summary_table(self, language):
+    def summary_table(
+        self, language: Literal["English", "French", "German", "Spanish"]
+    ):
         return self.chain.run(
             input="Synthesize all of this in Markdown table format, with the main philosophers' views. "
             "Just give the Markdown table output, nothing else. Keep it concise, as this will be displayed in a table. ",
