4ca8119ea3c9faddd8227a5798d5755a97b709f2
Daniel Altunay <daniel.altunay@decathlon.com>
2023-11-08 12:55:28 +0100

    test

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-import requests
+import os
+from langchain.llms import Replicate
 
-model_owner = "mistralai"
-model_name = "mistral-7b-instruct-v0.1"
-api_key = "r8_Ez1niBpNFVqMTv5gHRbc9ROhvNsCQo93ZCsPj"
+os.environ["CURL_CA_BUNDLE"] = ""
 
-response = requests.get(
-    url=f"https://api.replicate.com/v1/models/{model_owner}/{model_name}",
-    headers={"Authorization": f"Token {api_key}"},
+llm = Replicate(
+    model="mistralai/mistral-7b-instruct-v0.1:83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70",
+    replicate_api_token="r8_Ez1niBpNFVqMTv5gHRbc9ROhvNsCQo93ZCsPj",
 )
 
-print(response.json())
-print(response.ok)
+llm("What is life?")
